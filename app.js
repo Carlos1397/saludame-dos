82a0d353844f7cd2660e50779b837a6e5fd73b7f
Carlos Viera <pisis_13_3@hotmail.com>
2022-02-05 21:13:38 -0500

    update capitalizando

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+const e = require('express');
 const express = require('express');
 const res = require('express/lib/response');
 const port = 3000;
 const server = express();
 server.get('/makers/:nombre?', function (request, response) {
-    const { nombre } = request.params
-    const name = nombre.charAt(0).toUpperCase() + nombre.substring(1);
-    response.send(`<h1>Hola ${name}!</h1>`);
+    const { nombre } = request.params;
+    response.send(`<h1>Hola ${nombre[0].toUpperCase() + nombre.slice(1)}!</h1>`);
 });
 server.listen(port, function () {
     console.log(`http://localhost:${port}/makers/juan`)
