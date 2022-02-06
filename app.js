533d607e67f4a266697339b4af42112151d31c5d
Carlos Viera <pisis_13_3@hotmail.com>
2022-02-06 00:39:57 -0500

    add method POST

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ server.get('/makers/:nombre?', function (request, response) {
     const { nombre } = request.params;
     response.send(`<h1>Hola ${nombre[0].toUpperCase() + nombre.slice(1)}!</h1>`);
 });
+app.get("/makers/:name", (req, res) => {
+    let name = req.params.name;
+    name = name.replace(/^\w/, c => c.toUpperCase());
+
+    res.send(`<h1>Hola ${name}!</h1>`);
+});
+
 server.listen(port, function () {
     console.log(`http://localhost:${port}/makers/juan`)
 });
