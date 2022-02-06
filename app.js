8414fadbe6a9ea84647c4b642c84e82d61ae252c
Carlos Viera <pisis_13_3@hotmail.com>
2022-02-05 20:58:51 -0500

    Add Capitalizando

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const port = 3000;
 const server = express();
 server.get('/makers/:nombre?', function (request, response) {
     const { nombre } = request.params
-    response.send(`<h1>Hola ${nombre}!</h1>`);
+    const name = nombre.charAt(0).toUpperCase() + nombre.substring(1);
+    response.send(`<h1>Hola ${name}!</h1>`);
 });
 server.listen(port, function () {
     console.log(`http://localhost:${port}/makers/juan`)
